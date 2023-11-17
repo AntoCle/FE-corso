e9bf95bb1b44b5eaa595befa71ca0718b08ef45b
AntoCle <clemenzaantonino96@gmail.com>
2023-11-17 13:56:43 +0100

    appunti e primi es Javascript

---
+++ EserciziJs/Esercizio1.js
@@ -0,0 +1,17 @@
+/* n1 Dichiarare variabili numeriche 
+ed eseguire tutti i calcoli 
+(somma, divisione, moltiplicazione, sottrazione) 
+e stampare i risultati */
+
+/* n2 Scrivi un programma che utilizzi un ciclo 
+per stampare i numeri da 1 a 10 
+
+n2.1 Esegui la stampa da 1 a 20 tramite un altro strumento*/
+
+
+/* n3 Tramite un istruzione condizionale determinate se un numero
+ è positivo, negativo o zero */
+ 
+
+ /* n4 Esercizio 4: Creare un programma che utilizzi un do-while
+  per chiedere ad un utente un numero segreto fino a che non lo indovina */
