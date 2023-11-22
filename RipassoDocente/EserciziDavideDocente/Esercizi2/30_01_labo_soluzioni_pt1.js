85e7337669147ba81129287119ab21de9da8b32d
AntoCle <clemenzaantonino96@gmail.com>
2023-11-22 14:07:27 +0100

    commit esercizi Manuek

---
+++ RipassoDocente/EserciziDavideDocente/Esercizi2/30_01_labo_soluzioni_pt1.js
@@ -0,0 +1,67 @@
+
+// Exercise 30_01_01.00
+/* let age = 22;
+
+if (age >= 14 && age <= 90) {
+    console.log("Age is between 14 and 90 (inclusive)")
+} else {
+    console.log("Age is not between 14 and 90 (inclusive)")
+} */
+
+// Exercise 30_01_01.05
+/* let myVariable = true;
+console.log("Value of myVariable", myVariable);
+
+if (myVariable) {
+    console.log("The variable is true.");
+}
+
+if (!myVariable) {
+    console.log("The variable is false.");
+} else {
+    console.log("The variable is not false.");
+}
+myVariable = !myVariable; */
+
+// Exercise 30_01_01.10
+/* let age = 100;
+
+if (!(age >= 14 && age <= 90)) {
+    console.log("Age is not between 14 and 90 (inclusive) - with NOT operator");
+}
+
+if (age < 14 || age > 90) {
+    console.log("Age is not between 14 and 90 (inclusive) - without NOT operator");
+} */
+
+// Exercise 30_01_01.15
+/* let userAge = prompt("Please enter your age:");
+userAge = Number(userAge);
+
+let message = "";
+
+if (userAge >= 21) {
+    message = "You can enter to the venue and purchase alcohol.";
+} else if (userAge >= 19) {
+    message = "You can enter to the venue but cannot purchase alcohol.";
+} else {
+    message = "You cannot access to the venue.";
+}
+console.log(message); */
+
+// Exercise 30_01_01.20
+//if (-1 || 0) alert('first'); // yes
+//if (-1 && 0) alert('second'); // no
+//if (null || -1 && 1) alert('third'); // yes
+
+// Exercise 30_01_01.25
+/* let isValidId = true;
+
+
+let message = isValidId ?
+    "You are allowed to enter the venue."
+    : "You are not allowed to enter the venue."; */
+
+/* condizione da verificare ?
+    "risultato se la condizione è verificata"
+    : "risultato se la condizione non è verificata" */
