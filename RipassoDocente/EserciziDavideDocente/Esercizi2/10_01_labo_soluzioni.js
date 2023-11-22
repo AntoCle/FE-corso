2e0fe4301d225265f53f4b0aa5db1c45a4cdb3e8
AntoCle <clemenzaantonino96@gmail.com>
2023-11-22 12:02:16 +0100

    Commit esercizi docente

---
+++ RipassoDocente/EserciziDavideDocente/Esercizi2/10_01_labo_soluzioni.js
@@ -0,0 +1,140 @@
+// Exercise 10.01.00 ----------------------------
+
+// console.log("hello world!");
+
+// Exercise 10.01.05 ----------------------------
+
+// const a = 1;
+// const b = 2;
+// const c = 3;
+// const d = 4;
+// const e = 5;
+
+// console.log("sum = " + (a + b + c + d + e));
+// console.log("avg = " + (a + b + c + d + e) / 5);
+
+// Exercise 10.01.10 ----------------------------
+
+// let str1 = "Laurence";
+// let str2 = "Svekis";
+// let val1 = undefined;
+// let val2 = null;
+// let myNum = 1000;
+// console.log(typeof str1, typeof str2, typeof val1, typeof val2, typeof myNum);
+
+// Exercise 10.01.20 ----------------------------
+
+// let name = "Simone";
+// let age = 26;
+// let js = true;
+
+// console.log(
+//   "Hello, my name is " +
+//     name +
+//     ", I am " +
+//     age +
+//     " years old and I can code JavaScript: " +
+//     js
+// );
+// console.log(
+//   `Hello, my name is ${name}, I am ${age} years old and I can code JavaScript: ${js}`
+// );
+
+// Exercise 10.01.30 ----------------------------
+
+// let distance = 209.2142;
+// console.log(
+//   `The distance of ${distance / 1.60934} kms is equal to ${distance} miles`
+// );
+
+// Exercise 10.01.40 ----------------------------
+
+// let height = 70; // inches
+// let weight = 120; // pounds
+// let cmHeight = height * 2.54; // cm
+// let mHeight = cmHeight / 100; // m
+// let kgWeight = weight / 2.2046; // kg
+// console.log(cmHeight);
+// console.log(kgWeight);
+// console.log(kgWeight / mHeight ** 2);
+
+// Exercise 10.01.50 ----------------------------
+
+// const seconds = 12560;
+// const hours = Math.floor(seconds / 3600);
+// const minutes = Math.floor((seconds % 3600) / 60);
+// const sec = seconds % 60;
+// // console.log(hours.toFixed(0));
+// // console.log(parseInt(hours));
+// // console.log(Math.floor(hours));
+// console.log(`${hours} hours, ${minutes} minutes and ${sec} seconds`);
+
+
+// Exercise 10.01.60
+
+/* let a = 5, b = 2, c = 'hello';
+
+console.log(a == 5); // true
+console.log(b == '2'); // true
+console.log(c == 'Hello'); // false
+console.log(a != 2); // true
+console.log(b != 'Hello'); // true
+console.log(a === 2); // false
+console.log(a === '2'); // false
+console.log(a !== 2); // true
+console.log(a !== '2'); // true
+console.log(b !== 'Hello'); // true
+console.log(a > 5); // false
+console.log(a < 2); // false
+console.log(a >= 5); // true
+console.log(a <= 5); // true */
+
+// Exercise 10.01.70
+
+//alert(null || 2 || undefined); // 2
+//alert( alert(1) || 2 || alert(3) ); // alert(1) e alert(2)
+//alert(1 && null && 2); // alert(null)
+//alert(alert(1) && alert(2)); // alert(1) alert(undefined)
+//alert(null || 2 && 3 || 4); // alert(3)
+
+//const c = "5"; // string
+//const c = 91; // number
+
+//let empty1 = undefined; // variabile non inizializzata
+//let empty2 = null; // variabile inizializzata
+
+/* let a = "Hello";
+a = "world";
+console.log(a); // world */
+
+/* let a = "world";
+let b = `Hello ${a}!`;
+console.log(b); // Hello world! */
+
+/* let a = "Hello";
+a = prompt("world");
+console.log(a);// input in prompt */
+
+//let result = 3 + 4 * 2 / 8; // 4
+
+/* let firstNum = 5;
+let secondNum = 10;
+firstNum++;
+secondNum--;
+let total = ++firstNum + secondNum;
+console.log(total);
+
+// 16
+
+let total2 = 500 + 100 / 5 + total--;
+console.log(total2);
+
+// 536 */
+
+/* const a = 5;
+const b = 10;
+console.log(a > 0 && b > 0); // true
+console.log(a == 5 && b == 4); // false
+console.log(true || false); // true
+console.log(a == 3 || b == 10); // true
+console.log(a == 3 || b == 7); // false */
